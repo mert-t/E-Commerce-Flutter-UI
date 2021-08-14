091519ca403d5be13886fefd7a52d658c23566ed
mert-t <temiz.mertt@gmail.com>
2021-08-14 11:30:38 +0300

    files are changed

--- lib/screens/sign_in.dart/components/sign_in_body.dart
+++ lib/screens/sign_in.dart/components/sign_in_body.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_flutter_ui/components/custom_surfix_icon.dart';
 import 'package:e_commerce_flutter_ui/constants.dart';
 import 'package:e_commerce_flutter_ui/size_config.dart';
 import 'package:flutter/material.dart';
@@ -48,6 +49,7 @@ class _SignFormState extends State<SignForm> {
       child: Column (
         children: [
           TextFormField(
+            keyboardType:TextInputType.emailAddress,
             decoration: InputDecoration(
               labelText: "Email",
               hintText: "Enter your email",
@@ -61,24 +63,3 @@ class _SignFormState extends State<SignForm> {
   }
 }
 
-class CustomSuffixIcon extends StatelessWidget {
-  const CustomSuffixIcon({
-    Key key,
-   @required this.svgIcon,
-  }) : super(key: key);
-
-  final String svgIcon;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(
-        0,
-        getProportionateScreenWidth(20),
-        getProportionateScreenWidth(20),
-        getProportionateScreenWidth(20),
-      ),
-      child: SvgPicture.asset(svgIcon,height:getProportionateScreenWidth(18) ,),
-    );
-  }
-}
