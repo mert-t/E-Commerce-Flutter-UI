7415b995ec05c5791b9149a2d553dea2b26effb5
mert-t <temiz.mertt@gmail.com>
2021-08-15 11:04:29 +0300

    app bar bugs are fixed

--- lib/screens/sign_in.dart/sign_in_screen.dart
+++ lib/screens/sign_in.dart/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_flutter_ui/constants.dart';
 import 'package:e_commerce_flutter_ui/screens/sign_in.dart/components/sign_in_body.dart';
 import 'package:flutter/material.dart';
 class SignInScreen extends StatelessWidget {
@@ -7,7 +8,8 @@ static String routeName ="/sign_in";
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign In"),
+        title: Text("Sign In",style:TextStyle(color: kSecondaryColor) , ),
+        centerTitle:true,
       ),
       body: SignInBody(),
     );
