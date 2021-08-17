eeb67c19d46c962906c9c2bbb7dd0f7b4a285713
mert-t <temiz.mertt@gmail.com>
2021-08-17 10:11:31 +0300

    Success Screen bugs are fixed

--- lib/screens/login_success/components/login_success_body.dart
+++ lib/screens/login_success/components/login_success_body.dart
@@ -5,37 +5,34 @@ import 'package:flutter/material.dart';
 class LoginSuccessBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:  EdgeInsets.fromLTRB(30, 10, 0, 10),
-      child: Column(
-        
-        children: [
-          SizedBox(height: SizeConfig.screenHeight * 0.04),
-          Image.asset(
-            
-            "assets/images/success.png",
-            height: SizeConfig.screenHeight * 0.4, //40%
+    return Column(
+      
+      children: [
+        SizedBox(height: SizeConfig.screenHeight * 0.04),
+        Image.asset(
+          
+          "assets/images/success.png",
+          height: SizeConfig.screenHeight * 0.5, //40%
+        ),
+        SizedBox(height: SizeConfig.screenHeight * 0.08),
+        Text(
+          "Login Success",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(30),
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
-          SizedBox(height: SizeConfig.screenHeight * 0.08),
-          Text(
-            "Login Success",
-            style: TextStyle(
-              fontSize: getProportionateScreenWidth(30),
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
+        ),
+        Spacer(),
+        SizedBox(
+          width: SizeConfig.screenWidth * 0.6,
+          child: DefaultButton(
+            text: "Back to home",
+            press: () {},
           ),
-          Spacer(),
-          SizedBox(
-            width: SizeConfig.screenWidth * 0.6,
-            child: DefaultButton(
-              text: "Back to home",
-              press: () {},
-            ),
-          ),
-          Spacer(),
-        ],
-      ),
+        ),
+        Spacer(),
+      ],
     );
   }
 }
