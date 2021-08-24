1d0f00790551449606c6623c869a1e4d66c959dd
mert-t <temiz.mertt@gmail.com>
2021-08-24 15:32:49 +0300

    specialOffers widget is added

--- lib/screens/home/components/home_body.dart
+++ lib/screens/home/components/home_body.dart
@@ -1,15 +1,9 @@
-import 'dart:ui';
-
-import 'package:e_commerce_flutter_ui/constants.dart';
-import 'package:e_commerce_flutter_ui/screens/home/components/search_field.dart';
+import 'package:e_commerce_flutter_ui/screens/home/components/special_offers.dart';
 import 'package:e_commerce_flutter_ui/size_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
-import 'icon_btn_w_counter.dart';
 
 class HomeBody extends StatelessWidget {
   @override
@@ -24,10 +18,11 @@ class HomeBody extends StatelessWidget {
             DiscountBanner(),
             SizedBox(height: getProportionateScreenWidth(30)),
             Categories(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            SpecialOffers()
           ],
         ),
       ),
     );
   }
 }
-
---
+++ lib/screens/home/components/special_offers.dart
@@ -0,0 +1,108 @@
+import 'package:e_commerce_flutter_ui/screens/home/components/section_title.dart';
+import 'package:flutter/material.dart';
+
+import '../../../size_config.dart';
+
+class SpecialOffers extends StatelessWidget {
+  const SpecialOffers({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(text: "Special for You", press: () {}),
+        SizedBox(
+          height: getProportionateScreenWidth(20),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SpecialOfferCard(
+                image: "assets/images/Image Banner 2.png",
+                category: "Smartphones",
+                numOfBrands: 18,
+                press: () {},
+              ),
+              SpecialOfferCard(
+                image: "assets/images/Image Banner 3.png",
+                category: "Fashion",
+                numOfBrands: 24,
+                press: () {},
+              ),
+              SizedBox(width: getProportionateScreenWidth(20)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SpecialOfferCard extends StatelessWidget {
+  const SpecialOfferCard({
+    Key key,
+    @required this.category,
+    @required this.image,
+    @required this.numOfBrands,
+    @required this.press,
+  }) : super(key: key);
+
+  final String category, image;
+  final int numOfBrands;
+  final GestureTapCallback press;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
+      child: SizedBox(
+        width: getProportionateScreenWidth(242),
+        height: getProportionateScreenWidth(100),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Stack(
+            children: [
+              Image.asset(
+                image,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.center,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFF343434).withOpacity(0.4),
+                        Color(0xFF343434).withOpacity(0.15),
+                      ]),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(15),
+                    vertical: getProportionateScreenWidth(10)),
+                child: Text.rich(
+                  TextSpan(
+                    style: TextStyle(color: Colors.white),
+                    children: [
+                      TextSpan(
+                        text: "$category\n",
+                        style: TextStyle(
+                          fontSize: getProportionateScreenWidth(18),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(text: "$numOfBrands Brands"),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
