3c3290582850e1a8b319ce186c1ea43bc942dcfb
mert-t <temiz.mertt@gmail.com>
2021-08-24 10:32:15 +0300

    Categories widget is added

--- lib/screens/home/components/home_body.dart
+++ lib/screens/home/components/home_body.dart
@@ -1,4 +1,3 @@
-
 import 'dart:ui';
 
 import 'package:e_commerce_flutter_ui/constants.dart';
@@ -7,6 +6,7 @@ import 'package:e_commerce_flutter_ui/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
 import 'icon_btn_w_counter.dart';
@@ -20,15 +20,14 @@ class HomeBody extends StatelessWidget {
           children: [
             SizedBox(height: getProportionateScreenWidth(20)),
             HomeHeader(),
-             SizedBox(height: getProportionateScreenWidth(30)),
+            SizedBox(height: getProportionateScreenWidth(30)),
             DiscountBanner(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            Categories(),
           ],
         ),
       ),
     );
   }
 }
-
-
-
 
