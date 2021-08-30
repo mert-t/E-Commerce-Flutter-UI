389ecd2c5ffa09f78da742bed46ceb55307bbb67
mert-t <temiz.mertt@gmail.com>
2021-08-30 18:41:08 +0300

    cart screen is done

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,5 +1,9 @@
+import 'package:e_commerce_flutter_ui/components/default_button.dart';
+import 'package:e_commerce_flutter_ui/constants.dart';
 import 'package:e_commerce_flutter_ui/models/cart.dart';
+import 'package:e_commerce_flutter_ui/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'components/cart_body.dart';
 
@@ -11,6 +15,7 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(context),
       body: CartBody(),
+      bottomNavigationBar: CheckOurCard(),
     );
   }
 
@@ -22,10 +27,94 @@ class CartScreen extends StatelessWidget {
             "Your Cart",
             style: TextStyle(color: Colors.black),
           ),
-          Text("${demoCarts.length} items",style:Theme.of(context).textTheme.caption,)
+          Text(
+            "${demoCarts.length} items",
+            style: Theme.of(context).textTheme.caption,
+          )
         ],
       ),
       centerTitle: true,
+    );
+  }
+}
+
+class CheckOurCard extends StatelessWidget {
+  const CheckOurCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: getProportionateScreenWidth(15),
+        horizontal: getProportionateScreenWidth(30),
+      ),
+      // height: 174,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(
+              0,
+              -15,
+            ),
+            blurRadius: 20,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize:MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: getProportionateScreenWidth(40),
+                  width: getProportionateScreenWidth(40),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF5F6F9),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SvgPicture.asset("assets/icons/receipt.svg"),
+                ),
+                Spacer(),
+                Text("Add voucher code"),
+                const SizedBox(width: 10),
+                Icon(Icons.arrow_forward_ios, size: 12, color: kTextColor),
+              ],
+            ),
+            SizedBox(height: getProportionateScreenHeight(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Total:\n",
+                    children: [
+                      TextSpan(
+                        text: "\$337.15",
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: getProportionateScreenWidth(190),
+                  child: DefaultButton(text: "Check Out",
+                  press: (){},
+      
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/cart/components/cart_body.dart
+++ lib/screens/cart/components/cart_body.dart
@@ -3,17 +3,49 @@ import 'package:e_commerce_flutter_ui/models/Product.dart';
 import 'package:e_commerce_flutter_ui/models/cart.dart';
 import 'package:e_commerce_flutter_ui/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'cart_item_card.dart';
 
-class CartBody extends StatelessWidget {
+class CartBody extends StatefulWidget {
+  @override
+  State<CartBody> createState() => _CartBodyState();
+}
+
+class _CartBodyState extends State<CartBody> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CartItemCard(cart: demoCarts[0]),
-      ],
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ListView.builder(
+        itemCount: demoCarts.length,
+        itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Dismissible(
+            key: Key(demoCarts[index].product.id.toString()),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Color(0xFFFFE6E6),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  Spacer(),
+                  SvgPicture.asset("assets/icons/Trash.svg"),
+                ],
+              ),
+            ),
+            onDismissed: (direction) {
+              setState(() {
+                demoCarts.removeAt(index);
+              });
+            },
+            child: CartItemCard(cart: demoCarts[index]),
+          ),
+        ),
+      ),
     );
   }
 }
-
